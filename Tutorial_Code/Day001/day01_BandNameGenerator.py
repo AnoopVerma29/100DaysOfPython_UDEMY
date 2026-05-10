df74a2ef523317af6e74ad61d6eb65844ba4451d
Anoop Verma <avlearning29@gmail.com>
2026-05-10 21:00:54 +0530

    Add files via upload

---
+++ Tutorial_Code/Day001/day01_BandNameGenerator.py
@@ -0,0 +1,19 @@
+# Day 01 of 100 Days of Python
+# Band Name Generator
+#
+# Instructions:
+# Create a band name generator that takes the name of the city you grew up in and the
+# name of your pet and combines them to form a band name. For example, if you grew up in
+# "New York" and your pet's name is "Fluffy", the band name would be "New York Fluffy".
+# Anoop Verma, 6-May-2026
+#
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+
+band_name = city + " " + pet
+
+print("Your band name could be " + band_name)
+
+print()
+
---
+++ Tutorial_Code/Day001/task.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print()
+#
+print("Hello World!\nHello World!\nHello World!")
+print()
+#
+print("Hello " + "Anoop")
+print()
+#
+print("Hello" + " " + "Anoop")
+print()
+
+
